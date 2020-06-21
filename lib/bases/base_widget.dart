e6e6df785cf388d9261e12b172fb74e7f0b58780
DLTruongc <dltruong.c@gmail.com>
2020-06-21 22:28:21 +0700

    first commit

---
+++ lib/bases/base_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:fluttermessenger/shares/app_color.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+
+class PageContainer extends StatelessWidget {
+  final String title;
+  final Widget child;
+
+  final List<SingleChildWidget> bloc;
+  final List<SingleChildWidget> di;
+  final List<Widget> actions;
+
+  PageContainer({this.title, this.bloc, this.di, this.actions, this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ...di,
+        ...bloc,
+      ],
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            title,
+            style: TextStyle(color: AppColor.blue),
+          ),
+          actions: actions,
+        ),
+        body: child,
+      ),
+    );
+  }
+}
